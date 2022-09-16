9be02513837915c6aa662d6cab245c8d11f07c68
emsaks <emsaks@gmail.com>
2022-09-16 12:37:37 +0000

    more fleshing out

--- block_persist.c
+++ block_persist.c
@@ -11,60 +11,86 @@
 
 struct bp_dev {
 	struct device dev;
+	struct list_head bpd_list;
+	int timeout;
+	int suspend;
+	int disabled;
+	char blockdev[PATH_MAX];
+	struct mutex lock;
+	struct gendisk * disk;
 };
 // ll of devices
 
+#define dev_to_bp(dev) (container_of(dev, sturct bp_dev, dev))
+
+static LIST_HEAD(bp_devices);
+
+static ssize_t disabled_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct bp_dev * bpd = dev_to_bp(dev);
+	return sysfs_emit(buf, "%s\n", bpd->disabled ? "1" : "0");
+}
+
+static ssize_t disabled_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	return 0;
+}
+static DEVICE_ATTR_RW(disabled);
+
 static ssize_t suspend_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-
+	struct bp_dev * bpd = dev_to_bp(dev);
+	return sysfs_emit(buf, "%s\n", bpd->suspend ? "1" : "0");
 }
 
 static ssize_t suspend_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-
+	return 0;
 }
 static DEVICE_ATTR_RW(suspend);
 
 static ssize_t blockdev_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-
+	return 0;
 }
 
 static ssize_t blockdev_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-    
+    return 0;
 }
 static DEVICE_ATTR_RW(blockdev);
 
 static ssize_t port_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-
+	return 0;
 }
 
 static ssize_t port_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-    
+    return 0;
 }
 static DEVICE_ATTR_RW(port);
 
 static ssize_t timeout_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-
+	struct bp_dev * bpd = dev_to_bp(dev);
+	return sysfs_emit(buf, "%d\n", bpd->timeout);
 }
 
 static ssize_t timeout_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-    
+    return 0;
 }
 static DEVICE_ATTR_RW(timeout);
 
 static ssize_t delete_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-    
+    return 0;
 }
 static DEVICE_ATTR_WO(delete);
 
 static struct attribute *bp_dev_attrs[] = {
+	&dev_attr_disabled.attr,
 	&dev_attr_delete.attr,
 	&dev_attr_blockdev.attr,
 	&dev_attr_suspend.attr,
@@ -73,20 +99,65 @@ static struct attribute *bp_dev_attrs[] = {
 	NULL,
 };
 
-static const struct attribute_group bp_dev_attr_grp = {
-	.attrs = bp_dev_attrs,
-};
+ATTRIBUTE_GROUPS(bp_dev);
 
-static const struct attribute_group *bp_dev_group[] = {
-	&bp_dev_attr_grp,
-	NULL,
-};
+static int brd_alloc(const char * name)
+{
+	struct bp_device *bpd;
+	struct gendisk *disk;
+	char buf[DISK_NAME_LEN];
+	int err = -ENOMEM;
+
+
+	bpd = kzalloc(sizeof(*bpd), GFP_KERNEL);
+	if (!bpd)
+		return -ENOMEM;
+
+	list_add_tail(&bpd->bpd_list, &bp_devices);
+
+	mutex_init(&bpd->lock);
+
+	// todo: check available
+	snprintf(buf, DISK_NAME_LEN, name);
+	
+	disk = bpd->disk = blk_alloc_disk(NUMA_NO_NODE);
+	if (!disk)
+		goto out_free_dev;
+
+	disk->major			= bp_major;
+	disk->first_minor	= i * max_part; // todo
+	disk->minors		= 1;
+	disk->fops			= &bp_fops;
+	disk->private_data	= bpd;
+
+	strlcpy(disk->disk_name, buf, DISK_NAME_LEN);
+	set_capacity(disk, 0);
+	
+
+	/* Tell the block layer that this is not a rotational device */
+	blk_queue_flag_set(QUEUE_FLAG_NONROT, disk->queue);
+	blk_queue_flag_clear(QUEUE_FLAG_ADD_RANDOM, disk->queue);
+
+	bpd->dev.groups = bp_dev_group;
+
+
+	err = add_disk(disk);
+	if (err)
+		goto out_cleanup_disk;
+
+	return 0;
+
+out_cleanup_disk:
+	put_disk(disk);
+out_free_dev:
+	list_del(&bpd->bpd_list);
+	kfree(bpd);
+	return err;
+}
 
 static int bp_create(const char * name)
 {
     struct bp_dev * dev;
-
-    dev->dev.groups = bp_dev_group;
     
 }
 
@@ -111,6 +182,12 @@ static void bp_cleanup(void)
     // remove devices
 } 
 
+static struct platform_driver bp_driver = {
+	.driver = {
+		   .name = "block_persist",
+	},
+};
+
 static int bp_major;
 static int __init bp_init(void)
 {
@@ -122,9 +199,15 @@ static int __init bp_init(void)
         goto out_free;
 	}
 
+	err = platform_driver_register(&bp_driver);
+	if (err)
+		goto out_unreg_blkdev;
+	
 	pr_info("block_persist: module loaded\n");
 	return 0;
 
+out_unreg_blkdev:
+	unregister_blkdev(bp_major, "block_persist");
 out_free:
 	bp_cleanup();
 
