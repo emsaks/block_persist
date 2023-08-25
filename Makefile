280e569ab3664d771b2acc8930cc2e5ce3c77505
Eli Saks <emsaks@gmail.com>
2023-08-25 17:34:20 +0300

    makefile, fix compat

---
+++ Makefile
@@ -0,0 +1,19 @@
+VER := 1
+CFLAGS = -DMAKE_VER=$(VER)
+obj-m := blockthru$(VER).o
+blockthru$(VER)-objs := blockthru_main.o block_partscan.o persist.o
+KDIR := /lib/modules/$(shell uname -r)/build
+
+PWD := $(shell pwd)
+
+default:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+
+ins:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+	if [ -e /dev/bt$(VER)t ]; then echo 1 > /sys/block/bt$(VER)t/blockthru/delete; sleep 1; fi
+	if [ -e /sys/module/blockthru$(VER) ]; then rmmod blockthru$(VER).ko; fi
+	insmod blockthru$(VER).ko && printf bt$(VER)t > /sys/module/blockthru$(VER)/parameters/create
+
+clean:
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
--- compat.h
+++ compat.h
@@ -10,6 +10,9 @@
 #else
 #define GD_SUPPRESS_PART_SCAN GENHD_FL_NO_PART_SCAN
 #define GD_PS_STATE flags
+#define set_bit(flag, addr) *(addr) |= (flag)
+#define clear_bit(flag, addr) *(addr) &= ~(flag)
+#define test_bit(flag, addr) (*(addr) & (flag))
 #endif
 
 #ifndef BLK_STS_OFFLINE
@@ -17,10 +20,11 @@
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
-#define bt_bio_submit(bioarg) bt_bio_submit_compat(bioarg)
-static inline blkqc_t bt_submit_bio(struct bio * bio)
+#define bt_submit_bio(bioarg) bt_submit_bio_compat(bioarg)
+static void bt_submit_bio_compat(struct bio * bio);
+static inline blk_qc_t (bt_submit_bio)(struct bio * bio)
 {
-	bt_bio_submit_compat(bio);
+	bt_submit_bio_compat(bio);
 	return BLK_QC_T_NONE;
 } 
 #endif
