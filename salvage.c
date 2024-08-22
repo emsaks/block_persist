e78dca340c5538069d55bcba676c4383d992fee3
emsaks <emsaks@gmail.com>
2024-08-22 17:18:02 +0000

    attempt salvage bugcheck

--- salvage.c
+++ salvage.c
@@ -21,10 +21,8 @@ void prep_bio(struct bio * bio)
 	bio_for_each_segment(bvec, bio, iter) {
 		magic.page = bvec.bv_page;
 		mem = bvec_kmap_local(&bvec);
-		for (magic.off = 0; magic.off < bvec.bv_len; magic.off += 512) {
+		for (magic.off = 0; magic.off < bvec.bv_len; magic.off += 512)
 			memcpy(mem+magic.off, &magic, sizeof(magic));
-		}
-
 		kunmap_local(mem);
 	}
 	
@@ -32,14 +30,11 @@ void prep_bio(struct bio * bio)
 
 size_t salvage_bio(struct bio * bio)
 {
-	size_t salvaged = 0;
-	int in_error = 1;
-	int salvaged_segments = 0;
-
 	void * mem;
 	struct bio_vec bvec;
 	struct bvec_iter iter;
-	
+	size_t salvaged = 0;
+
 	if (bio->bi_opf != REQ_OP_READ)
 		return 0;
 
@@ -47,25 +42,33 @@ size_t salvage_bio(struct bio * bio)
 		magic.page = bvec.bv_page;
 		mem = bvec_kmap_local(&bvec);
 		for (magic.off = 0; magic.off < bvec.bv_len; magic.off += 512) {
-			if (memcmp(mem, &magic, sizeof(magic))) {
-				salvaged += 512;
-				if (in_error) {
-					in_error = 0;
-					++salvaged_segments;
-				}
-			} else {
-				in_error = 1;
-			}
+			if (!memcmp(mem, &magic, sizeof(magic)))
+				break;
+			salvaged += 512;
 		}
 
 		kunmap_local(mem);
+		if (magic.off < bvec.bv_len)
+			break;
 	}
 
 	if (salvaged) {
-		pr_warn("Salvaged %zu bytes in %i segments.\n", salvaged, salvaged_segments);
+		struct bio_vec * bvec = bio_last_bvec_all(bio);
+		magic.page = bvec->bv_page;
+		mem = bvec_kmap_local(bvec);
+		magic.off = bvec->bv_len - 512;
+		if (memcmp(mem, &magic, sizeof(magic)))
+			bvec = NULL;
+		kunmap_local(mem);
+
+		if (!bvec) {
+			pr_warn("Salvage bug? Final sector was modified even though bio has error");
+		} else {
+			pr_warn("Salvaged %zu bytes.\n", salvaged);
+			total_salvaged_bytes += salvaged;
+		}
 	}
 
-	total_salvaged_bytes += salvaged;
 	return salvaged;
 }
 
